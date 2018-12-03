90a353102c49159ed3894917bc5a393287270ce9
Seila Gonzalez <seila.gonzalez@matrix.msu.edu>
2018-12-03 05:30:16 -0500

    adding changes for constants

--- header.php
+++ header.php
@@ -1,7 +1,14 @@
 <?php
 require_once('config.php');
+//includes all the php files from wikiconstants directory
+foreach(glob('wikiconstants' . "/*.php") as $file){
+    require_once $file;
+}
+require_once('functions/explorefunctions.php');
+
 globaljsvars;
 print globaljsvars;
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,7 +26,7 @@ print globaljsvars;
         <!-- select2 cdn links -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-        
+
     </head>
 
     <body>
@@ -36,16 +43,16 @@ print globaljsvars;
                 </div>
                 <div class="rightnav">
                     <ul class="nav-menu">
-                        <li class="nav-item"><img class="search-icon" src="<?php echo BASE_URL;?>/assets/images/Search.svg"/><a class="nav-link unselected" id="search" href="<?php echo BASE_URL;?>search">Search</a></li>
+                        <li class="nav-item"><img class="search-icon" src="<?php echo BASE_URL;?>/assets/images/Search.svg"/><a class="nav-link unselected" id="search" href="<?php echo BASE_URL;?>search/">Search</a></li>
                         <li class="nav-item drop-link">
-                            <a class="nav-link unselected" id="explore" href="<?php echo BASE_URL;?>explore">Explore</a>
+                            <a class="nav-link unselected" id="explore" href="<?php echo BASE_URL;?>explore/">Explore</a>
                             <span class="drop-carat"><img src="<?php echo BASE_URL;?>/assets/images/Arrow.svg"/></span>
                             <ul class="sub-list">
                                 <li class="subwrap" id="explore-sub">
-                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>explorePeople">People</a>
-                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>exploreEvents">Events</a>
-                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>explorePlaces">Places</a>
-                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>exploreSources">Sources</a>
+                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>explorePeople/">People</a>
+                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>exploreEvents/">Events</a>
+                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>explorePlaces/">Places</a>
+                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>exploreSources/">Sources</a>
                                 </li>
                             </ul>
                         </li>
@@ -60,16 +67,16 @@ print globaljsvars;
                                 </li>
                             </ul>
                         </li>
-                        <li class="nav-item"><a class="nav-link unselected" id="stories" href="<?php echo BASE_URL;?>stories">Stories</a></li>
-                        <li class="nav-item"><a class="nav-link unselected" id="projects" href="<?php echo BASE_URL;?>projects">Projects</a></li>
+                        <li class="nav-item"><a class="nav-link unselected" id="stories" href="<?php echo BASE_URL;?>stories/">Stories</a></li>
+                        <li class="nav-item"><a class="nav-link unselected" id="projects" href="<?php echo BASE_URL;?>projects/">Projects</a></li>
                         <li class="nav-item drop-link">
-                            <a class="nav-link unselected" id="about" href="<?php echo BASE_URL;?>about">About</a>
+                            <a class="nav-link unselected" id="about" href="<?php echo BASE_URL;?>about/">About</a>
                             <span class="drop-carat"><img src="<?php echo BASE_URL;?>/assets/images/Arrow.svg"/></span>
                             <ul class="sub-list">
                                 <li class="subwrap" id="about-sub">
-                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>getInvolved">Get Involved</a>
-                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>ourPartners">Our Partners</a>
-                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>contactUs">Contact Us</a>
+                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>getInvolved/">Get Involved</a>
+                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>ourPartners/">Our Partners</a>
+                                    <a class="nav-sublink" href="<?php echo BASE_URL;?>contactUs/">Contact Us</a>
                                 </li>
                             </ul>
                         </li>
