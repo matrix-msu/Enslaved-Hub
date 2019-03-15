cedceedb0904dd40b8377dbd65a49e653a9b0122
Josh Christ <christj2@msu.edu>
2019-03-15 18:20:53 +0000

    Update config.dist.php

--- config.dist.php
+++ config.dist.php
@@ -1,7 +1,10 @@
 <?php
+//user defined
+define("BASE_URL",  "http://dev2.matrix.msu.edu/enslaved/");
+define("BASE_PATH",  "/matrix/dev/public_html/enslaved/");
+define('BASE_WIKI_URL','https://sandro-16.matrix.msu.edu/');
 
 //project specific urls - you should never use relative paths
-define("BASE_URL",  "http://dev2.matrix.msu.edu/enslaved/");
 define("BASE_JS_URL", BASE_URL . "assets/javascripts/");
 define("BASE_AJAX_URL", BASE_URL . "ajax/");
 define("BASE_VIEW_URL", BASE_URL . "views/");
@@ -11,7 +14,6 @@ define("BASE_MODULE_URL", BASE_URL . "modules/");
 define("BASE_LEAFLET_URL", BASE_URL . "assets/leaflet/");
 
 //project specific file paths - you should never use relative paths
-define("BASE_PATH",  "/matrix/dev/public_html/enslaved/");
 define("BASE_LIB_PATH", BASE_PATH . "lib/");
 define("BASE_FUNCTIONS_PATH", BASE_PATH . "functions/");
 define("BASE_VIEW_PATH", BASE_PATH . "views/");
@@ -22,10 +24,9 @@ define("KORA_BASE_URL", "https://enslaved.kora3.matrix.msu.edu/");
 define("KORA_SEARCH_URL", KORA_BASE_URL . "api/search");
 define("KORA_FILES_URL", KORA_BASE_URL . "public/app/files/");
 
-define('API_URL','https://sandro-33.matrix.msu.edu/sparql?query=');
-define('BLAZEGRAPH_URL','https://sandro-33.matrix.msu.edu/namespace/wdq/sparql');
-define('BASE_WIKI_URL','https://sandro-16.matrix.msu.edu/');
-define('WIKI_ENTITY_URL',  BASE_WIKI_URL . 'entity/');
+define('BLAZEGRAPH_URL', BASE_WIKI_URL.'namespace/wdq/sparql');
+define('API_URL', BASE_WIKI_URL.'sparql?query=');
+define('WIKI_ENTITY_URL', BASE_WIKI_URL.'entity/');
 
 //kora project information - change these for your project
 define('TOKEN', 'FjOx8EcNE2HS1y3rJiJJ7ha4');
