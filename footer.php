fca1bf19f20f9458c8e81b4692efebf5daa55c4e
Drew Schineller <schinel3@msu.edu>
2019-05-15 13:31:52 -0400

    Changed image urls in footer to use constants

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer id="footer">
     <div class="footerwrap">
-        <a href="https://mellon.org" target="_blank"><img src="assets/images/Footer-mellon.svg"  alt="The Andew W. Mellon Foundation Logo" /></a>
-      <a href="https://www.matrix.msu.edu" target="_blank"><img src="assets/images/Footer-Matrix-Logo-White.svg"  alt="Matrix: The Center for Digital Humanities and Social Sciences Logo"/></a>
-      <a href="https://msu.edu" target="_blank"><img src="assets/images/Footer-MSUWordmark.svg"  alt="Michigan State University Logo"/></a>
-      <a href="http://socialscience.msu.edu" target="_blank"><img src="assets/images/Footer-collegeSocialScience.svg" alt="College of Social Science Logo" /></a>
+        <a href="https://mellon.org" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-mellon.svg"  alt="The Andew W. Mellon Foundation Logo" /></a>
+      <a href="https://www.matrix.msu.edu" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-Matrix-Logo-White.svg"  alt="Matrix: The Center for Digital Humanities and Social Sciences Logo"/></a>
+      <a href="https://msu.edu" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-MSUWordmark.svg"  alt="Michigan State University Logo"/></a>
+      <a href="http://socialscience.msu.edu" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-collegeSocialScience.svg" alt="College of Social Science Logo" /></a>
         
         
     </div>
