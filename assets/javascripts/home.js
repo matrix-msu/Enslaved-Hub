ac32501d0966d44aab4eaf0f4c025adefcf4dc36
noah.girard <girardno@msu.edu>
2019-09-30 14:58:52 +0000

    moved inline php funtions into ajax calls in a new home.js file

---
+++ assets/javascripts/home.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    // Fill in the counters
+    $.ajax({
+        url: BASE_URL + "api/getHomePageCounters",
+        type: "GET",
+        'success': function (data) {
+            result_array = JSON.parse(data);
+            for (var key in result_array) {
+                $("#count-"+key).html(result_array[key])
+            }
+        }
+    });
+
+    // Create the 2 stories cards
+    $.ajax({
+        url: BASE_URL + "api/blazegraph",
+        type: "GET",
+        data: {
+            preset: 'stories',
+            filters:  {limit: 2},
+            templates: ['homeCard']
+        },
+        'success': function (data) {
+            result_array = JSON.parse(data);
+            console.log(result_array);
+            result_array['homeCard'].forEach(function (card) {
+                $(card).appendTo("#stories-list");
+            });
+        }
+    });
+
+    // Create the 2 projects cards
+    $.ajax({
+        url: BASE_URL + "api/blazegraph",
+        type: "GET",
+        data: {
+            preset: 'projects',
+            filters:  {limit: 2},
+            templates: ['homeCard']
+        },
+        'success': function (data) {
+            result_array = JSON.parse(data);
+            result_array['homeCard'].forEach(function (card) {
+                $(card).appendTo("#projects-list");
+            });
+        }
+    });
+});
