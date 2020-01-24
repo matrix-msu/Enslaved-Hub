19670f736d2c4f0f59b27eb965ed6b452e016a6c
Iris Kim <kimiris@msu.edu>
2020-01-24 16:23:06 +0000

    404 page

--- views/404.php
+++ views/404.php
@@ -1,6 +1,23 @@
-<div class="container main">
-    <div class="container middlewrap">
-        <h1>404 :(((</h1>
-    </div>
+<div class="top">
+  <div class="topwrap">
+  </div>
 </div>
 
+<div class="middle-card">
+  <div class="sort-middle-text1">
+    <h>404 PAGE NOT FOUND</h><br>
+  </div>
+  <div class="sort-middle-text2">
+    <h>Sorry, we couldn't find that page.</h><br>
+  </div>
+  <div class="sort-middle-text3">
+    <h>Please try searching or visit the home page.</h><br>
+  </div>
+
+  <div class="button">
+    <a href='https://robbie.dev.matrix.msu.edu/~kimiris/enslaved/'>Go Home</a>
+  </div>
+</div>
+
+<div class="container page">
+</div>
