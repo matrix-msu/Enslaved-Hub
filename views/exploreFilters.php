95c702bee99fe2ccdf83caa991dae1ba161a4372
andrewmk <andrewmk@msu.edu>
2020-02-06 21:37:54 +0000

    Issue:368 Now lists filters on Explore By sections for Place and Source

--- views/exploreFilters.php
+++ views/exploreFilters.php
@@ -12,7 +12,7 @@ $upperWithSpaces = ucwords(str_replace("_", " ", EXPLORE_FILTER));
 </div>
 <!-- explore by -->
 <div class="explore-by">
-    
+
     <ul class="cards">
       <?php
       $lowerWithDashes = strtolower(str_replace(" ", "_", EXPLORE_FILTER));
