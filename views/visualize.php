c76b8ad8207fd0116d4bcb4f50f145858dfbf50b
kimhyeun <kimhyeun@msu.edu>
2020-02-12 20:12:24 +0000

    changes

---
+++ views/visualize.php
@@ -0,0 +1,34 @@
+<!-- Author: Hyeungsuk Kim-->
+<!-- Visualize page-->
+<?php $cache_data = Json_GetData_ByTitle("Visualize");
+?>
+
+<div class="container header visualize-header">
+  <img class="header-background visualize-page" src="<?php echo BASE_URL;?>assets/images/<?php echo $bg[$randIndex];?>" alt="Enslaved Background Image">
+
+    <div class="container middlewrap">
+        <h1><?php echo $cache_data['title'] ?></h1>
+  </div>
+
+</div>
+
+<div class="container info">
+  <div class="container infowrap">
+    <p>Brief info on Section. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  Lorem ipsum dolor tempor aliqua  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum  consectetur a tempor incididunt ut labore et dolore magna Lorem ipsum dolor tempor aliqua  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum  consectetur  Lorem ipsum.</p>
+  </div>
+</div>
+
+<div class="cardwrap visualize">
+  <li class="card">
+    <img src="assets/images/BySpace.svg">
+    <p>By Space</p>
+  </li>
+  <li class="card">
+    <img src="assets/images/ByTime.svg">
+    <p>By Time</p>
+  </li>
+  <li class="card">
+    <img src="assets/images/ByData.svg">
+    <p>By Data</p>
+  </li>
+</div>
