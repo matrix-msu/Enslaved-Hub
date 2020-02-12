ea2c174e328467cc851c6742d171b00e6c12898a
Alicia Sheill <asheill@msu.edu>
2020-02-12 15:36:12 +0000

    Have Stories pull from Kora; comment out hardcoded text

--- views/home.php
+++ views/home.php
@@ -115,7 +115,8 @@ if (isset($story2Images[0])){
             <div class="row">
                 <div class="col">
                     <h2 class="section-title">Stories</h2>
-                    <p class="paragraph">Learn about both prominent and everyday people associated with the historical slave trade through short biographical sketches.</p>
+                    <p class="paragraph"><?php echo array_key_exists("Stories", $cache_data) ? $cache_data["Stories"] : "" ?></p>
+                   <!--<p class="paragraph">Learn about both prominent and everyday people associated with the historical slave trade through short biographical sketches.</p>-->
                     <p class="mt-lg"><a class="text-link" href="<?php echo BASE_URL;?>stories">View All Stories</a></p>
                 </div>
 
