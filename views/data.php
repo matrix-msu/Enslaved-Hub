b838f704cfef95569bcc795f9f29ecaa0a12135d
Alicia Sheill <asheill@msu.edu>
2020-04-01 17:43:29 +0000

    add paragraph class before the description

--- views/data.php
+++ views/data.php
@@ -16,7 +16,7 @@
 <!-- info container-->
 <div class="container info">
     <div class="container infowrap">
-        <p><?php echo $cache_data['descr'] ?></p>
+        <p class="paragraph"><?php echo $cache_data['descr'] ?></p>
     </div>
 </div>
 <div class="cardwrap contributors">
