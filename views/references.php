12265aa407e0ef048ffcbbcb3de8361917c0eea4
Alicia Sheill <asheill@msu.edu>
2020-04-01 18:45:48 +0000

    Remove commented out code

--- views/references.php
+++ views/references.php
@@ -24,26 +24,5 @@
         <p class="paragraph"><?php echo $cache_data['descr'] ?></p>
     </div>
 </div>
-        <!-- <p><?php echo $cache_data["descr"] ?></p> -->
-
     </div>
 </div>
-
-
-<!-- text sections -->
-<!--
-<div class="container about-text">
-    <div class="container textwrap">
-        <h2>Subheader Tincidunt praesent semper feugiat nibh sed pulvinar. </h2>
-        <p>Brief Info on Project. Tincidunt praesent semper feugiat nibh sed pulvinar. Elementum nisi quis eleifend quam adipiscing vitae proin. Suscipit tellus mauris a diam maecenas sed enim. Neque egestas congue quisque egestas diam in arcu cursus euismod. Bibendum at varius vel pharetra. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Tellus at urna condimentum mattis pellentesque. Tincidunt augue interdum velit euismod in pellentesque massa placerat. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. </p>
-        <br>
-        <p>Et malesuada fames ac turpis. Orci eu lobortis elementum nibh. Nibh venenatis cras sed felis. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Egestas sed sed risus pretium quam vulputate dignissim suspendisse in. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Faucibus et molestie ac feugiat sed lectus. Vitae tortor condimentum lacinia quis vel eros donec ac odio. Tincidunt eget nullam non nisi est sit amet. Pharetra convallis posuere morbi leo urna molestie at elementum. Quis varius quam quisque id diam vel. Pulvinar sapien et ligula ullamcorper. Aliquam ultrices sagittis orci a scelerisque purus semper eget. Pellentesque pulvinar pellentesque habitant morbi tristique. Eu lobortis elementum nibh tellus molestie nunc non. </p>
-    </div>
-    <div class="container textwrap">
-        <h2>Subheader Tincidunt praesent semper feugiat nibh sed pulvinar. </h2>
-        <p>Brief Info on Project. Tincidunt praesent semper feugiat nibh sed pulvinar. Elementum nisi quis eleifend quam adipiscing vitae proin. Suscipit tellus mauris a diam maecenas sed enim. Neque egestas congue quisque egestas diam in arcu cursus euismod. Bibendum at varius vel pharetra. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Tellus at urna condimentum mattis pellentesque. Tincidunt augue interdum velit euismod in pellentesque massa placerat. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. </p>
-        <br>
-        <p>Et malesuada fames ac turpis. Orci eu lobortis elementum nibh. Nibh venenatis cras sed felis. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Egestas sed sed risus pretium quam vulputate dignissim suspendisse in. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Faucibus et molestie ac feugiat sed lectus. Vitae tortor condimentum lacinia quis vel eros donec ac odio. Tincidunt eget nullam non nisi est sit amet. Pharetra convallis posuere morbi leo urna molestie at elementum. Quis varius quam quisque id diam vel. Pulvinar sapien et ligula ullamcorper. Aliquam ultrices sagittis orci a scelerisque purus semper eget. Pellentesque pulvinar pellentesque habitant morbi tristique. Eu lobortis elementum nibh tellus molestie nunc non. </p>
-    </div>
-</div>
- -->
