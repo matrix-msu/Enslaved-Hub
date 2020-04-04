c51deaa7fdca0a84559961fa8adc14e2db81f1db
Alicia Sheill <asheill@msu.edu>
2020-04-04 18:52:59 +0000

    remove commented out text

--- views/support-our-mission.php
+++ views/support-our-mission.php
@@ -20,19 +20,3 @@
         <p><?php echo $cache_data['descr'] ?></p>
     </div>
 </div>
-
-<!-- text sections -->
-<!--    <div class="container support-text">
-
-		<p>As you can imagine, we have a lot to do to make the Enslaved project a success and there are many ways you can help.</p>
-
-		<h2>Publish data you have collected.</h2>
-		<p>Go to the <a href="" target="_blank"> Enslaved Publishing Platform</a> to find out more.</p>
-
-		<h2>Let us know about datasets and resources to which we can connect.</h2>
-		<p>Email us at <a href="mailto:enslaved.org@gmail.com">enslaved.org@gmail.com</a>.</p>
-
-		<h2>Help fund the project with your support.</h2>
-      <a class="view-more" href="https://givingto.msu.edu/gift/index.cfm?desi_code=AN04&desig_descrip=College%20of%20Social%20Science%20Matrix&CFID=757810&CFTOKEN=9513505b20b6aba4-E4C5564B-E081-30E7-1CB17EB97DADED75&jsessionid=f0301f221bdbcd94341b217b2976632c1f2f" target="_blank">Donate to Enslaved</a>
--->
-</div>
