8f9cbd4bb93e409601ba1a1fab0bdc38dd473ad6
izzy.barraza <barraza2@msu.edu>
2020-05-18 16:43:06 +0000

    Added files

---
+++ assets/javascripts/visualizeByData.js
@@ -0,0 +1,88 @@
+var pie_age = "https://kibana.enslaved.org/app/kibana#/visualize/edit/c7b63e90-4829-11ea-803f-c5c32c8e80c2?embed=true&_g=()";
+var pie_ageBS = "https://kibana.enslaved.org/app/kibana#/visualize/edit/2ac8cd60-5f1b-11ea-aee9-d7fe312c992a?embed=true&_g=()";
+var bar_15 = "https://kibana.enslaved.org/app/kibana#/visualize/edit/9c6096f0-6246-11ea-9c31-37fdb38a3ce2?embed=true&_g=(filters%3A!())";
+var line_ages = "https://kibana.enslaved.org/app/kibana#/visualize/edit/92b3dac0-58ca-11ea-bd95-ef0656102a8f?embed=true&_g=(filters%3A!())";
+var bar_age = "https://kibana.enslaved.org/app/kibana#/visualize/edit/db186de0-58ce-11ea-bd95-ef0656102a8f?embed=true&_g=(filters%3A!())";
+
+$(document).ready( function() {
+  $('#chart-type').select2({
+      placeholder: "Select Chart Type"
+  });
+  $('#chart-field').select2({
+      placeholder: "Select Chart Field"
+  });
+  $('.select2-selection--multiple').append('<span class="select2-selection__arrow" role="presentation"></span>');
+
+  addFields();
+
+});
+
+$( "#chart-type" ).change(function() {
+  var type = $('#chart-type').val();
+  var field = $('#chart-field').val();
+
+  if(type == "pie" && field == "ages"){
+    $('iframe').attr("src", pie_age);
+  }
+  else if(type == "pie" && field == "agesBySex"){
+    $('iframe').attr("src", pie_ageBS);
+  }
+  else if(type == "bar" && field == "15yo"){
+    $('iframe').attr("src", bar_15);
+  }
+  else if(type == "line" && field == "ages"){
+    $('iframe').attr("src", line_ages);
+  }
+  else if(type == "bar" && field == "ages"){
+    $('iframe').attr("src", bar_age);
+  }
+  else{
+    $('iframe').attr("src", "");
+  }
+  addFields();
+});
+$( "#chart-field" ).change(function() {
+  var type = $('#chart-type').val();
+  var field = $('#chart-field').val();
+
+  if(type == "pie" && field == "ages"){
+    $('iframe').attr("src", pie_age);
+  }
+  else if(type == "pie" && field == "agesBySex"){
+    $('iframe').attr("src", pie_ageBS);
+  }
+  else if(type == "bar" && field == "15yo"){
+    $('iframe').attr("src", bar_15);
+  }
+  else if(type == "line" && field == "ages"){
+    $('iframe').attr("src", line_ages);
+  }
+  else if(type == "bar" && field == "ages"){
+    $('iframe').attr("src", bar_age);
+  }
+  else{
+    $('iframe').attr("src", "");
+  }
+
+});
+
+function addFields(){
+  var type = $('#chart-type');
+
+  $("#chart-field").empty();
+
+  if (type.val() == "pie"){
+    $('iframe').attr("src", pie_age);
+    $("#chart-field").append("<option value='ages'>Ages</option>");
+    $("#chart-field").append("<option value='agesBySex'>Ages - Sex</option>");
+  }
+  if (type.val() == "bar"){
+    $('iframe').attr("src", bar_age);
+    $("#chart-field").append("<option value='ages'>Ages</option>");
+    $("#chart-field").append("<option value='15yo'>Ages - 15 years old</option>");
+  }
+  if (type.val() == "line"){
+    $('iframe').attr("src", line_ages);
+    $("#chart-field").append("<option value='ages'>Ages</option>");
+  }
+}
---
+++ views/visualizeByData.php
@@ -0,0 +1,40 @@
+<!-- Author: Izzy Barraza-->
+<!-- Visualize by data page-->
+<?php $cache_data = Json_GetData_ByTitle("Visualize");
+?>
+
+<div class="container header visualizeByData-header">
+  <div class="image-container visualizeByData-page image-only">
+    <img class="header-background visualizeByData-page" src="<?php echo BASE_URL;?>assets/images/<?php echo $bg[$randIndex];?>" alt="Enslaved Background Image">
+      <div class="container middlewrap">
+        <h4 class="last-page-header"><a id="last-page" href="<?php echo BASE_URL;?>visualize"><span id="previous-title">Visualize / </span></a><span id="current-title">Visualize By Data</span></h4>
+          <h1><?php echo "Visualize By Data" ?></h1>
+    </div>
+    <div class="image-background-overlay"></div>
+  </div>
+</div>
+
+<div class="container info">
+  <div class="search-section">
+    <div class="inputwrap">
+      <label for="chart_type">Chart Type</label>
+      <select class="s2-single" id="chart-type" name="chart_type">
+        <option value="pie">Pie</option>
+        <option value="bar">Bar</option>
+        <option value="line">Line</option>
+      </select>
+    </div>
+    <div class="inputwrap">
+      <label for="chart_field">Chart Field</label>
+      <select class="s2-single" id="chart-field" name="chart_field">
+        <option value="ages">Ages</option>
+        <option value="agesBySex">Ages - Sex</option>
+        <option value="15yo">Ages - 15 years old</option>
+      </select>
+    </div>
+  </div>
+  <div class="container infowrap">
+    <iframe id="data-frame"></iframe>
+  </div>
+</div>
+<script src="<?php echo BASE_URL;?>assets/javascripts/visualizeByData.js"></script>
