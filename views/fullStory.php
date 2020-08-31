08b775b45e000135531ba9208cf136a4109fbf44
truchan <truchan@msu.edu>
2020-08-31 13:28:56 +0000

    Commented out Story Connections and Featured Stories

--- views/fullStory.php
+++ views/fullStory.php
@@ -186,7 +186,7 @@ else {
     </div>
 </main>
 <!-- Story Connections -->
-<div class="record-connections">
+<!--<div class="record-connections">
     <div class="connectionwrap">
     <h2>Story Connections</h2>
     <div class="categories">
@@ -229,9 +229,10 @@ else {
         <a class="search-all"></a>
     </div>
 </div>
-</div>
-<!-- Related Stories-->
-<div class="container card-column related-card">
+</div> -->
+
+<!-- Related Stories -->
+<!--<div class="container card-column related-card">
     <div class="container cardheader-wrap">
         <h2 class="column-header">Featured in these Stories</h2>
     </div>
@@ -251,8 +252,11 @@ else {
             </li>
         </ul>
     </div>
-</div>
+</div> -->
 <!-- Extra Info -->
+</br>
+</br>
+</br>
 <div class="extra-info">
     <div class="copyright">
         <h2>Copyright</h2>
