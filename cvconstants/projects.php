c3ad379f15fa12dea335e08cbcb46a1ebf52f640
Seila Gonzalez <seilage@msu.edu>
2020-11-28 10:54:55 +0000

    modification on project constants

--- cvconstants/projects.php
+++ cvconstants/projects.php
@@ -4,19 +4,17 @@ const projects=[
     'Free Blacks Database' => 'Q455',
     'Hutchins Center for African & African American Research' => 'Q280811',
     'Voyages: The Trans-Atlantic Slave Trade Database' => 'Q9866',
-    'African Origins' => 'Q9867',
     'Maranhão Inventories Slave Database' => 'Q11585',
     "Legacies of British Slave-ownership" => "Q376",
     "Contested Freedom" => "Q624610"
 ];
 
 const qprojects=[
-  "Q89" => "Liberated Africans",
-  "Q129" => "Voyages: The Trans-Atlantic Slave Trade Database",
-  "Q162" => "Slave Biographies",
-  "Q325" => "African American National Biography (AANB)",
-  "Q340" => "Slave Societies Digital Archive",
+  "Q11586" => "Louisiana Slave Database",
+  "Q9866" => "Voyages: The Trans-Atlantic Slave Trade Database",
+  "Q455" => "Free Blacks Database",
+  "Q280811" => "Hutchins Center for African & African American Research",
   "Q376" => "Legacies of British Slave-ownership",
-  "Q383" => "Freedom Narratives",
+  "Q11585" => "Maranhão Inventories Slave Database",
   "Q624610" => "Contested Freedom"
 ];
