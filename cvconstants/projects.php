0c7af25f0eaa15efffe318827941dbec44a52154
Seila Gonzalez <seilage@msu.edu>
2020-11-28 11:03:36 +0000

    change on project name

--- cvconstants/projects.php
+++ cvconstants/projects.php
@@ -1,7 +1,7 @@
 <?php
 const projects=[
     'Louisiana Slave Database' => 'Q11586',
-    'Free Blacks Database' => 'Q455',
+    'Free Blacks Database New Orleans 1840-1860' => 'Q455',
     'Hutchins Center for African & African American Research' => 'Q280811',
     'Voyages: The Trans-Atlantic Slave Trade Database' => 'Q9866',
     'Maranhão Inventories Slave Database' => 'Q11585',
@@ -12,7 +12,7 @@ const projects=[
 const qprojects=[
   "Q11586" => "Louisiana Slave Database",
   "Q9866" => "Voyages: The Trans-Atlantic Slave Trade Database",
-  "Q455" => "Free Blacks Database",
+  "Q455" => "Free Blacks Database New Orleans 1840-1860",
   "Q280811" => "Hutchins Center for African & African American Research",
   "Q376" => "Legacies of British Slave-ownership",
   "Q11585" => "Maranhão Inventories Slave Database",
