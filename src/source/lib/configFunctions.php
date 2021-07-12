cc3a9fdbd050d941ab31298943a1747e6d042f45
Josh Christ <christj2@msu.edu>
2021-07-12 20:47:19 +0000

    editing autodeploy

--- src/source/lib/configFunctions.php
+++ src/source/lib/configFunctions.php
@@ -56,7 +56,6 @@ function Kora_GetNavigationData()
 	// put navigations to navContents.json file
 	file_put_contents( "./source/cache/navContents.json", json_encode($navs));
 	define("NAV_CONTENT", json_encode($navs));
-	echo NAV_CONTENT;die;
 
 	return json_encode("updated");
 }
@@ -67,10 +66,8 @@ function Json_GetNavigationData()
 	// Read data from Json cache File
 	if(file_exists(BASE_PATH . "cache/navContents.json") ){ //exists
 		$navContents = file_get_contents(BASE_PATH . "cache/navContents.json");
-		echo 'file exits';die;
 	}else{  //doesn't exist
 		$navContents = NAV_CONTENT;
-		echo 'file not exits';die;
 	}
 	$navContents = json_decode($navContents, true);
 	$formattedContents = array();
