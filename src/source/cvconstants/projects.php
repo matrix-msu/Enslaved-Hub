ceb4c50a88f06bf1176396e5d84a68f278af2b20
Josh Christ <christj2@msu.edu>
2021-08-13 15:31:36 +0000

    copy multiple commits to newest cvconstant/projects.php

--- src/source/cvconstants/projects.php
+++ src/source/cvconstants/projects.php
@@ -2,9 +2,9 @@
 const projects=[
     'Louisiana Slave Database' => 'Q11586',
     'Free Blacks Database New Orleans 1840-1860' => 'Q455',
-    'Hutchins Center for African & African American Research' => 'Q280811',
+    'Biographies of the Enslaved Era' => 'Q280811',
     'Voyages: The Trans-Atlantic Slave Trade Database' => 'Q9866',
-    'Maranhão Inventories Slave Database' => 'Q11585',
+    'Maranhao Inventories Slave Database' => 'Q11585',
     "Legacies of British Slave-ownership" => "Q376",
     "Contested Freedom: Free Persons of Color in Savannah, GA, 1823-1842" => "Q624610",
     "CSI Dixie" => "Q490726",
@@ -13,17 +13,18 @@ const projects=[
     "Family Search" => "Q811272",
     "Mortality in the South, 1850" =>"Q856062",
     "African Burials and Residences in Rio de Janeiro, 1874-1899" => "Q871865",
-    "Take Them in Families: The Enslaved People of Casa Bianca Plantation, Florida" =>"Q734407",
-    "They Had Names: Representations of the Enslaved in Liberty County, Georgia, Estate Inventories, 1762-1865" => "Q886231"
+    "Take Them In Families" =>"Q734407",
+    "They Had Names: Representations of the Enslaved in Liberty County, Georgia, Estate Inventories, 1762-1865" => "Q886231",
+    "Advertising Gender Slave Database: Escravos de ganho in Rio de Janeiro, 1850-1880" => "Q907421"
 ];
 
 const qprojects=[
   "Q11586" => "Louisiana Slave Database",
   "Q9866" => "Voyages: The Trans-Atlantic Slave Trade Database",
   "Q455" => "Free Blacks Database New Orleans 1840-1860",
-  "Q280811" => "Hutchins Center for African & African American Research",
+  "Q280811" => "Biographies of the Enslaved Era",
   "Q376" => "Legacies of British Slave-ownership",
-  "Q11585" => "Maranhão Inventories Slave Database",
+  "Q11585" => "Maranhao Inventories Slave Database",
   "Q624610" => "Contested Freedom: Free Persons of Color in Savannah, GA, 1823-1842",
   "Q490726" => "CSI Dixie",
   "Q624576" => "Free Africans of Brazil",
@@ -31,6 +32,11 @@ const qprojects=[
   "Q811272" => "Family Search",
   "Q856062" => "Mortality in the South, 1850",
   "Q871865" => "African Burials and Residences in Rio de Janeiro, 1874-1899",
-  "Q734407" => "Take Them in Families: The Enslaved People of Casa Bianca Plantation, Florida",
-  "Q886231" => "They Had Names: Representations of the Enslaved in Liberty County, Georgia, Estate Inventories, 1762-1865"
+  "Q734407" => "Take Them In Families",
+  "Q886231" => "They Had Names: Representations of the Enslaved in Liberty County, Georgia, Estate Inventories, 1762-1865",
+  "Q907421" => "Advertising Gender Slave Database: Escravos de ganho in Rio de Janeiro, 1850-1880"
+];
+
+const projectsPrettyLabels=[
+    'Maranhao Inventories Slave Database' => 'Maranhão Inventories Slave Database'
 ];
