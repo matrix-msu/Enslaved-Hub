d715f52723e1f53530a2cae94c19ddc5d0d9ff5b
Josh Christ <christj2@msu.edu>
2021-08-30 17:19:24 +0000

    update config

--- src/config.dist.php
+++ src/config.dist.php
@@ -16,19 +16,40 @@ require_once( "./source/lib/koraSearchRemote.php");
 //generate the webpages file
 Kora_GetNavigationData();
 
+// Get all Stories using KORA_Search
+// $fields =  ['Title', 'Featured', 'Images'];
+// $fields = ['Title', 'Images', 'Caption', 'Text', 'Resources', 'Source', 'Creator', 'Contributor', 'Timeline', 'Story_Associator','Contributing Institution', 'Connection'];
+$clause = new KORA_Clause("Display", "=", "True");
+$stories = KORA_Search(TOKEN, PID, STORY_SID, $clause, "ALL");
+unset($stories["count"]);
+
+$fileName= "./source/assets/javascripts/searchData.js";
+$script = "var allStoriesRecords = JSON.parse('".addslashes(json_encode(array_values($stories), true))."');";
+file_put_contents($fileName, $script);
+$gzScript = gzencode($script);
+file_put_contents($fileName.'.gz', $gzScript);
+
+$storyKids = array();
+foreach($stories as $kid => $record){
+    $storyKids[] = array(
+        'title' => $kid,
+        'content' => $kid
+    );
+}
+
+
+define("ALL_STORIES", $stories);
+
 return [
     'production' => false,
     'baseUrl' => '/~christj2/enslaved-static/build_local',
     'title' => 'Enslaved Static',
     'description' => 'Test project for website builder',
-    // 'collections' => [
-    //     'events' => [
-    //         'extends' => '_layouts.post',
-    //         'path' => 'record/event/{title}',
-    //         'items' => [
-    //             ['title'=>'3title', 'content'=>'3content'],
-    //             ['title'=>'4title', 'content'=>'4content']
-    //         ],
-    //     ],
-    // ],
+    'collections' => [
+        'stories' => [
+            'extends' => '_layouts.story',
+            'path' => 'fullStory/{title}',
+            'items' => $storyKids,
+        ]
+    ]
 ];
