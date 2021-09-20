e756908d16de2243a82b62c1a9cfc0bc15db2ff8
Josh Christ <christj2@msu.edu>
2021-09-20 14:23:06 +0000

    copy of Seila's commit on master for constants

--- src/source/cvconstants/projects.php
+++ src/source/cvconstants/projects.php
@@ -10,12 +10,13 @@ const projects=[
     "CSI Dixie" => "Q490726",
     "Free Africans of Brazil" => "Q624576",
     "Economics of American Negro Slavery Series" => "Q627623",
-    "Family Search" => "Q811272",
+    // "Family Search" => "Q811272",
     "Mortality in the South, 1850" =>"Q856062",
     "African Burials and Residences in Rio de Janeiro, 1874-1899" => "Q871865",
     "Take Them In Families" =>"Q734407",
     "They Had Names: Representations of the Enslaved in Liberty County, Georgia, Estate Inventories, 1762-1865" => "Q886231",
-    "Advertising Gender Slave Database: Escravos de ganho in Rio de Janeiro, 1850-1880" => "Q907421"
+    "Advertising Gender Slave Database: Escravos de ganho in Rio de Janeiro, 1850-1880" => "Q907421",
+    "A Year of Slavery: Jornal do Commercio, 1840" => "Q912129"
 ];
 
 const qprojects=[
@@ -29,12 +30,13 @@ const qprojects=[
   "Q490726" => "CSI Dixie",
   "Q624576" => "Free Africans of Brazil",
   "Q627623" => "Economics of American Negro Slavery Series",
-  "Q811272" => "Family Search",
+  // "Q811272" => "Family Search",
   "Q856062" => "Mortality in the South, 1850",
   "Q871865" => "African Burials and Residences in Rio de Janeiro, 1874-1899",
   "Q734407" => "Take Them In Families",
   "Q886231" => "They Had Names: Representations of the Enslaved in Liberty County, Georgia, Estate Inventories, 1762-1865",
-  "Q907421" => "Advertising Gender Slave Database: Escravos de ganho in Rio de Janeiro, 1850-1880"
+  "Q907421" => "Advertising Gender Slave Database: Escravos de ganho in Rio de Janeiro, 1850-1880",
+  "Q912129" => "A Year of Slavery: Jornal do Commercio, 1840"
 ];
 
 const projectsPrettyLabels=[
