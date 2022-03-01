a76250e9b38ea228d673fbdbcdb673b4b98e55bf
Alicia Sheill <asheill@msu.edu>
2022-03-01 14:22:52 +0000

    Update Q#s, remove Coordinate

--- src/source/cvconstants/placetypes.php
+++ src/source/cvconstants/placetypes.php
@@ -1,18 +1,17 @@
 <?php
 const placeTypes=[
-    'Cemetery or Burial Ground' => '',
-    'City, Town, or Village' => 'Q29',
-    'Continent' => 'Q52',
-    'Coordinate' => '',
-    'Country' => 'Q32',
-    'County or Parish' => 'Q520',
-    'Court' => '',
-    'Domicile' => '',
-    'Maroon Community' => '',
-    'Plantation' => '',
-    'Port' => '',
-    'Province' => 'Q31',
-    'Region' => 'Q333',
-    'Worship Site' => 'Q435',
-    'Other Place Type' => ''
+    'Cemetery or Burial Ground' => 'Q118',
+    'City, Town, or Village' => 'Q194',
+    'Continent' => 'Q321',
+    'Country' => 'Q222',
+    'County or Parish' => 'Q322',
+    'Court' => 'Q176',
+    'Domicile' => 'Q123',
+    'Maroon Community' => 'Q119',
+    'Plantation' => 'Q347',
+    'Port' => 'Q346',
+    'Province' => 'Q213',
+    'Region' => 'Q233',
+    'Worship Site' => 'Q284',
+    'Other Place Type' => 'Q258'
 ];
