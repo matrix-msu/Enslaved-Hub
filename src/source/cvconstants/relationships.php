b48a9ed716f382a0c133598fff4b19cbad9026ae
Alicia Sheill <asheill@msu.edu>
2022-03-03 16:53:25 +0000

    Update based upon https://lod.enslaved.org/wiki/Special:WhatLinksHere/Q457

--- src/source/cvconstants/relationships.php
+++ src/source/cvconstants/relationships.php
@@ -1,20 +1,27 @@
 <?php
 
 const relationship =[
-  "Enslaved Person" => "Q109",
-  "Enslaver or Owner" => "Q112",
-  "Employer" => "Q218",
-  "Employee" => "Q220",
-  "Spouse" => "Q253",
-  "Child" => "Q456",
-  "Parent" => "Q458",
-  "Sibling" => "Q466",
-  "Godparent" => "Q467",
-  "Godchild" => "Q468",
-  "Grandparent" => "Q469",
-  "Grandchild" => "Q470",
-  "Cousin" => "Q471",
-  "In-law" => "Q473",
-  "Aunt" => "Q474",
-  "Uncle" => "Q475"
+    "Child" => "Q456",
+    "Employee" => "Q220",
+    "Employer" => "Q218",
+    "Enslaved Person" => "Q109",
+    "Enslaver or Owner" => "Q112",
+    "Grandchild" => "Q470",
+    "Grandparent" => "Q469",
+    "Parent" => "Q458",
+    "Sibling" => "Q466",
+    "Spouse" => "Q253"
+];
+
+const qrelationship =[
+    "Q456" => "Child",
+    "Q220" => "Employee",
+    "Q218" => "Employer",
+    "Q109" => "Enslaved Person",
+    "Q112" => "Enslaver or Owner",
+    "Q470" => "Grandchild",
+    "Q469" => "Grandparent",
+    "Q458" => "Parent",
+    "Q466" => "Sibling",
+    "Q253" => "Spouse"  
 ];
