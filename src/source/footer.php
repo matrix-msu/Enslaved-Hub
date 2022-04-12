4d28c6f8f3df4296abfe0d9d14bd9a04b2405709
truchan <truchan@msu.edu>
2022-04-12 12:48:11 +0000

    Added all white ucr.png, adjusted layout to flow better on mobile

--- src/source/footer.php
+++ src/source/footer.php
@@ -1,16 +1,15 @@
 <footer id="footer">
     <div class="footerwrap">
       <div class="top">
-        <div class="top-top">
+
           <a href="https://mellon.org" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-mellon.svg"  alt="The Andew W. Mellon Foundation Logo" /></a>
           <a href="https://www.neh.gov" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-neh.svg"  alt="The National Endowment for the Humanities Logo" /></a>
           <a href="https://www.matrix.msu.edu" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-Matrix-Logo-White.svg"  alt="Matrix: The Center for Digital Humanities and Social Sciences Logo"/></a>
-        </div>
-        <div class="top-bottom">
+
           <a href="https://msu.edu" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-MSUWordmark.svg"  alt="Michigan State University Logo"/></a>
           <a href="https://umd.edu" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-UMD.svg" alt="University of Maryland Logo" /></a>
-          <a href="https://www.ucr.edu" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-ucr.svg" alt="University of California, Riverside Logo" /></a> 
-        </div>
+          <a href="https://www.ucr.edu" target="_blank"><img src="<?php echo BASE_IMAGE_URL;?>Footer-ucr.png" alt="University of California, Riverside Logo" /></a>
+
       </div>
 
       <div class="bottom">
