ccd48253c65ebc227b58f57c605bc2d4d2a91a4e
truchan <truchan@msu.edu>
2022-05-06 14:10:11 +0000

    Data cards, data search,and responsiveness

--- src/source/views/data.php
+++ src/source/views/data.php
@@ -15,9 +15,189 @@
 </div>
 <!-- info container-->
 <div class="container info">
+  <div class="container infowrap">
+
+    <p>The following projects are among those that have linked data currently available within Enslaved.org. Filter the projects using the search tools below, or jump to more information on the projects within Enslaved.org.</p>
+
+    <div class="data-search">
+      <form>
+        <label class="sr-only" for="data-search">Search for project by name or keyword</label>
+        <input class="input-field data-search" type="search" id="data-search" name="data-search" placeholder="Search for project by name or keyword">
+
+        <label class="sr-only" for="start-year">Start Year</label>
+        <input class="input-field start-year" type="text" id="start-year" name="start-year" placeholder="Start Year">
+        <p>To</p>
+
+        <label class="sr-only" for="end-year">End Year</label>
+        <input class="input-field end-year" type="text" id="end-year" name="end-year" placeholder="End Year">
+        <button type="submit">Search</button>
+      </form>
+    </div>
+
+  </div>
+
+  <div class="container infowrap">
+
+
+      <div class="card contributor">
+        <h2>Advertising Gender Slave Database: Escravos de ganho in Rio de Janeiro</h2>
+        <div class="details">
+          <div class="detail">
+            <h5>Dates</h5>
+            <p>1850-1880</p>
+          </div>
+          <div class="detail">
+            <h5>Contributor(s)</h5>
+            <p>Kari Zimmerman</p>
+          </div>
+          <div class="detail">
+            <h5>External Reference</h5>
+            <p><a href="https://doi.org/10.25971/wdxt-j508">https://doi.org/10.25971/wdxt-j508</a></p>
+          </div>
+        </div>
+        <div class="bottom">
+          <div class="connectionswrap">
+            <h5>Project Connections</h5>
+            <div class="connections">
+              <div class="card-icons">
+                <img src="<?php echo BASE_URL;?>assets/images/Person-dark.svg">
+                <span class="tooltip">Person</span>
+                <span>10</span>
+            </div>
+            <div class="card-icons">
+              <img src="<?php echo BASE_URL;?>assets/images/Place-dark.svg">
+              <span class="tooltip">Place</span>
+              <span>12</span>
+            </div>
+            <div class="card-icons">
+              <img src="<?php echo BASE_URL;?>assets/images/Event-dark.svg">
+              <span class="tooltip">Event</span>
+              <span>4</span>
+            </div>
+            <div class="card-icons">
+              <img src="<?php echo BASE_URL;?>assets/images/Source-dark.svg">
+              <span class="tooltip">Source</span>
+              <span>2</span>
+            </div>
+          </div>
+        </div>
+        <div class="buttons">
+          <a href="" class="button">View Project Data</a>
+          <a href="" class="button">View Project Visualization</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="card contributor">
+      <h2>Advertising Gender Slave Database: Escravos de ganho in Rio de Janeiro</h2>
+      <div class="details">
+        <div class="detail">
+          <h5>Dates</h5>
+          <p>1850-1880</p>
+        </div>
+        <div class="detail">
+          <h5>Contributor(s)</h5>
+          <p>Kari Zimmerman</p>
+        </div>
+        <div class="detail">
+          <h5>External Reference</h5>
+          <p><a href="https://doi.org/10.25971/wdxt-j508">https://doi.org/10.25971/wdxt-j508</a></p>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="connectionswrap">
+          <h5>Project Connections</h5>
+          <div class="connections">
+            <div class="card-icons">
+              <img src="<?php echo BASE_URL;?>assets/images/Person-dark.svg">
+              <span class="tooltip">Person</span>
+              <span>10</span>
+          </div>
+          <div class="card-icons">
+            <img src="<?php echo BASE_URL;?>assets/images/Place-dark.svg">
+            <span class="tooltip">Place</span>
+            <span>12</span>
+          </div>
+          <div class="card-icons">
+            <img src="<?php echo BASE_URL;?>assets/images/Event-dark.svg">
+            <span class="tooltip">Event</span>
+            <span>4</span>
+          </div>
+          <div class="card-icons">
+            <img src="<?php echo BASE_URL;?>assets/images/Source-dark.svg">
+            <span class="tooltip">Source</span>
+            <span>2</span>
+          </div>
+        </div>
+      </div>
+      <div class="buttons">
+        <a href="" class="button">View Project Data</a>
+        <a href="" class="button">View Project Visualization</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="card contributor">
+    <h2>Advertising Gender Slave Database: Escravos de ganho in Rio de Janeiro</h2>
+    <div class="details">
+      <div class="detail">
+        <h5>Dates</h5>
+        <p>1850-1880</p>
+      </div>
+      <div class="detail">
+        <h5>Contributor(s)</h5>
+        <p>Kari Zimmerman</p>
+      </div>
+      <div class="detail">
+        <h5>External Reference</h5>
+        <p><a href="https://doi.org/10.25971/wdxt-j508">https://doi.org/10.25971/wdxt-j508</a></p>
+      </div>
+    </div>
+    <div class="bottom">
+      <div class="connectionswrap">
+        <h5>Project Connections</h5>
+        <div class="connections">
+          <div class="card-icons">
+            <img src="<?php echo BASE_URL;?>assets/images/Person-dark.svg">
+            <span class="tooltip">Person</span>
+            <span>10</span>
+        </div>
+        <div class="card-icons">
+          <img src="<?php echo BASE_URL;?>assets/images/Place-dark.svg">
+          <span class="tooltip">Place</span>
+          <span>12</span>
+        </div>
+        <div class="card-icons">
+          <img src="<?php echo BASE_URL;?>assets/images/Event-dark.svg">
+          <span class="tooltip">Event</span>
+          <span>4</span>
+        </div>
+        <div class="card-icons">
+          <img src="<?php echo BASE_URL;?>assets/images/Source-dark.svg">
+          <span class="tooltip">Source</span>
+          <span>2</span>
+        </div>
+      </div>
+    </div>
+    <div class="buttons">
+      <a href="" class="button">View Project Data</a>
+      <a href="" class="button">View Project Visualization</a>
+    </div>
+  </div>
+</div>
+
+<hr>
+
+</div>
+
+
+
     <div class="container infowrap">
         <?php echo $cache_data['descr'] ?>
     </div>
+
+
+
 </div>
 <!-- <div class="cardwrap contributors">
     <?php echo $cache_data['dataSplit'] ?>
